378176cefa01fb52c36c0d099cd4bcdfbabe9445
siarasmall <siarasmall19@gmail.com>
2018-07-18 13:30:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @times_through = 0 
   
-    @number = params[:number].to_i
+    @number = params[:number]
     @phrase = params[:phrase]
     @repeated_phrase = [ ]
     loop do 
